15c374f0a81f7eaf91b98a8dac261c3981150c6d
IniyanKanmani <kiniyan002@gmail.com>
2024-03-18 11:50:41 +0000

    initial commit

---
+++ android/app/src/main/kotlin/com/nanosoft/stock_management_tool/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nanosoft.stock_management_tool
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const StockManagementToolApp());
+}
+
+class StockManagementToolApp extends StatelessWidget {
+  const StockManagementToolApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nanosoft Stock Management Tool',
+      home: Container(),
+    );
+  }
+}
